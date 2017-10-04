4ba851ca5f06ae0a396416790cceb6498c8c70fd
SnhrK <suna@sunaharakenyuunoMacBook-Pro.local>
2017-10-04 13:22:41 +0900

    update retrytime

--- aliyun/Common/Client/Client.php
+++ aliyun/Common/Client/Client.php
@@ -69,7 +69,7 @@ class Client {
      * @param integer $waittime   Delay execution
      * @return $this Client
      */
-    public function retryExecuteClient($request, $setter, $chkStatus, $oper = 'describe', $message = null, $maxRetryCount = 5, $waittime = 0) {
+    public function retryExecuteClient($request, $setter, $chkStatus, $oper = 'describe', $message = null, $maxRetryCount = 10, $waittime = 0) {
         $status = false;
         $retryCount = 0;
         if (empty($waittime)) $waittime = WaitTime::TIME;
